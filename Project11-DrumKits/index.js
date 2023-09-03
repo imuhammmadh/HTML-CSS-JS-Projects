0a8d00130c67b6817abdb1ab003dd469b107be1c
imuhammmadh <imuhammmadh@gmail.com>
2023-09-03 14:26:54 +0530

    .

--- Project11-DrumKits/index.js
+++ Project11-DrumKits/index.js
@@ -3,9 +3,22 @@ let drums = document.querySelector(".drums");
 kits.forEach((kits) => {
     let btnEl = document.createElement("button");
     btnEl.classList.add("btn");
-    btnEl.innerText = kits
+    btnEl.innerText = kits;
+    btnEl.style.backgroundImage = "url(images/" + kits + ".png)";
     drums.appendChild(btnEl);
     let audioEl = document.createElement("audio");
-    audioEl.src =
-        drums.appendChild(audioEl);
-})
+    audioEl.src = "sounds/" + kits + ".mp3"
+    drums.appendChild(audioEl);
+    btnEl.addEventListener("click", () => {
+        audioEl.play();
+    })
+    window.addEventListener("keydown", (event) => {
+        if (event.key == kits.slice(0, 1)) {
+            audioEl.play();
+            btnEl.style.transform = "scale(.9)";
+            setTimeout(() => {
+                btnEl.style.transform = "scale(1)";
+            }, 100);
+        }
+    })
+});
