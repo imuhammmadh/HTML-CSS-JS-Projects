ca2a14384047258149d5de0e719b53adbe045fa8
imuhammmadh <imuhammmadh@gmail.com>
2023-09-04 12:49:41 +0530

    .

--- Project13-ImageSlider/index.js
+++ Project13-ImageSlider/index.js
@@ -1,11 +1,15 @@
 let next = document.querySelector(".right");
 let previous = document.querySelector(".left");
 let images = document.querySelector(".images");
+let imagesNum = document.querySelectorAll("img");
 let currentImg = 1;
 next.addEventListener("click", () => {
     currentImg++
     updateImg();
 });
 function updateImg() {
+    if (currentImg > imagesNum.length) {
+        currentImg = 1;
+    }
     images.style.transform = `translateX(-${(currentImg - 1) * 500}px)`
 }
