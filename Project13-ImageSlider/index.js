924faf093205b2b96bdc7d14359ef78cfb5f1236
imuhammmadh <imuhammmadh@gmail.com>
2023-09-04 12:57:13 +0530

    .

--- Project13-ImageSlider/index.js
+++ Project13-ImageSlider/index.js
@@ -3,13 +3,27 @@ let previous = document.querySelector(".left");
 let images = document.querySelector(".images");
 let imagesNum = document.querySelectorAll("img");
 let currentImg = 1;
+let timeout;
 next.addEventListener("click", () => {
     currentImg++
+    clearTimeout(timeout);
     updateImg();
 });
+previous.addEventListener("click", () => {
+    currentImg--;
+    clearTimeout(timeout);
+    updateImg();
+})
+updateImg();
 function updateImg() {
     if (currentImg > imagesNum.length) {
         currentImg = 1;
+    } else if (currentImg < 1) {
+        currentImg = imagesNum.length
     }
-    images.style.transform = `translateX(-${(currentImg - 1) * 500}px)`
+    images.style.transform = `translateX(-${(currentImg - 1) * 500}px)`;
+    timeout = setTimeout(() => {
+        currentImg++;
+        updateImg();
+    }, 3000);
 }
