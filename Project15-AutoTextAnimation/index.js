9e08bf0afbd4897c63559709fde971d6566368f9
imuhammmadh <imuhammmadh@gmail.com>
2023-09-05 12:54:10 +0530

    .

---
+++ Project15-AutoTextAnimation/index.js
@@ -0,0 +1,17 @@
+let containerEl = document.querySelector(".container");
+let careers = ["Developer", "Freelancer", "Student", "Indian"];
+let careersIndex = 0;
+let characterIndex = 0;
+updateText();
+function updateText() {
+    characterIndex++
+    containerEl.innerHTML = `<h1>I am ${careers[careersIndex].slice(0, 1) == "I" ? "an" : "a"} ${careers[careersIndex].slice(0, characterIndex)}</h1>`;
+    if (characterIndex == careers[careersIndex].length) {
+        careersIndex++
+        characterIndex = 0
+    }
+    if (careersIndex === careers.length) {
+        careersIndex = 0
+    }
+    setTimeout(updateText, 400)
+}
