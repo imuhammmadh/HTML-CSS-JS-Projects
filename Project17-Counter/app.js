667fa128586aa12df2d9a992e5c9d841b9c3db51
imuhammmadh <imuhammmadh@gmail.com>
2023-09-07 19:01:09 +0530

    .

--- Project17-Counter/app.js
+++ Project17-Counter/app.js
@@ -6,20 +6,21 @@ let btns = document.querySelectorAll(".btn");
 btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
         let styles = e.currentTarget.classList;
-        if (styles.contains('decrease')) {
+        if (styles.contains("decrease")) {
             count--;
-        } else if (styles.contains('increase')) {
+        } else if (styles.contains("increase")) {
             count++;
         } else {
             count = 0;
         }
+
         if (count > 0) {
             value.style.color = "green";
         }
         if (count < 0) {
             value.style.color = "red";
         }
-        if (count = 0) {
+        if (count == 0) {
             value.style.color = "#222";
         }
         value.textContent = count;
