1421d284b3376a2581387180dfa1ac8a6f3ad030
imuhammmadh <imuhammmadh@gmail.com>
2023-09-09 14:08:29 +0530

    .

--- Project18-Reviews/app.js
+++ Project18-Reviews/app.js
@@ -30,11 +30,53 @@ const reviews = [
   },
 ];
 // select items
-let img = document.getElementById("person-img");
+let personImg = document.getElementById("person-img");
 let author = document.getElementById("author");
 let job = document.getElementById("job");
 let info = document.getElementById("info");
 
-let prevBtn = document.querySelector(".prev-btn")
-let nextBtn = document.querySelector(".next-btn")
-let randomBtn = document.querySelector(".random-btn")
+let prevBtn = document.querySelector(".prev-btn");
+let nextBtn = document.querySelector(".next-btn");
+let randomBtn = document.querySelector(".random-btn");
+
+//set starting item
+let currentItem = 0;
+
+//load initial item
+window.addEventListener("DOMContentLoaded", () => {
+  showPerson(currentItem);
+});
+
+//show person based on item
+function showPerson(person) {
+  let item = reviews[person];
+  personImg.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+//show next person
+nextBtn.addEventListener("click", () => {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+})
+
+//show prev button
+
+prevBtn.addEventListener("click", () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+})
+
+//show random
+randomBtn.addEventListener("click", () => {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem)
+})
