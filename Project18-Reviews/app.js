9f04b690cd6ad050474dd626ba07425697251611
imuhammmadh <imuhammmadh@gmail.com>
2023-09-09 13:44:08 +0530

    .

--- Project18-Reviews/app.js
+++ Project18-Reviews/app.js
@@ -30,3 +30,11 @@ const reviews = [
   },
 ];
 // select items
+let img = document.getElementById("person-img");
+let author = document.getElementById("author");
+let job = document.getElementById("job");
+let info = document.getElementById("info");
+
+let prevBtn = document.querySelector(".prev-btn")
+let nextBtn = document.querySelector(".next-btn")
+let randomBtn = document.querySelector(".random-btn")
