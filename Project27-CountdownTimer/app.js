0f39e78c24e21d422fbe5fad455bd6a4f9e42825
imuhammmadh <imuhammmadh@gmail.com>
2023-09-15 22:50:32 +0530

    .

--- Project27-CountdownTimer/app.js
+++ Project27-CountdownTimer/app.js
@@ -21,3 +21,18 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+let giveaway = document.querySelector('.giveaway')
+let deadline = document.querySelector('.deadline')
+let items = document.querySelectorAll('.deadline-format h4')
+
+let futureDate = new Date(2023, 8, 24, 11, 30, 0);
+let year = futureDate.getFullYear();
+let hours = futureDate.getHours();
+let minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+let date = futureDate.getDate();
+let day = futureDate.getDay();
+day = weekdays[day];
+month = months[month];
+
+giveaway.textContent = `giveaway ends on ${day},${date} ${month} ${year} ${hours}:${minutes}am`
