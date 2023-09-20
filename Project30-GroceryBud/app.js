708d3c8c9f1f4ae60bbf09610241c433386eb3b9
imuhammmadh <imuhammmadh@gmail.com>
2023-09-21 00:30:23 +0530

    Selecting Classes&Id's

--- Project30-GroceryBud/app.js
+++ Project30-GroceryBud/app.js
@@ -1,5 +1,11 @@
 // ****** SELECT ITEMS **********
-
+let alert = document.querySelector('.alert')
+let form = document.querySelector('.grocery-form')
+let grocery = document.getElementById('grocery')
+let submitBtn = document.querySelector('.submit-btn')
+let container = document.querySelector('.grocery-container')
+let list = document.querySelector('.grocey-list')
+let clearBtn = document.querySelector('.clear-btn')
 // edit option
 
 // ****** EVENT LISTENERS **********
