daacb5215c54b700d1eaba871e09a0baac05158d
imuhammmadh <imuhammmadh@gmail.com>
2023-12-03 11:07:40 +0530

    HTML&CSS Completed

---
+++ Project36-TicTacToe/index.js
@@ -0,0 +1,5 @@
+//Creating Elements
+let squares = document.querySelectorAll(".square")
+let xScore = document.querySelector(".x")
+let oScore = document.querySelector(".o")
+let chance = document.querySelector("p")
